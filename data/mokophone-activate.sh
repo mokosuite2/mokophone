b1c64f52fe5c7a24bdba72a9d2a436430e84fb4b
Daniele Ricci <daniele.athome@gmail.com>
2010-12-20 19:49:39 +0100

    Startup script: use session bus
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

--- data/mokophone-activate.sh
+++ data/mokophone-activate.sh
@@ -6,4 +6,4 @@ PARAMS=$2
 if [ "$1" == "" ]; then METHOD="Frontend"; fi
 if [ "$2" == "" ]; then PARAMS="string:phone"; fi
 
-dbus-send --system --dest=org.mokosuite.phone --type=method_call --print-reply /org/mokosuite/Phone org.mokosuite.Phone.$METHOD $PARAMS
+dbus-send --session --dest=org.mokosuite.phone --type=method_call --print-reply /org/mokosuite/Phone org.mokosuite.Phone.$METHOD $PARAMS
